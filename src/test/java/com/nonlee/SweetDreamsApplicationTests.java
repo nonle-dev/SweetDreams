31dcbc01b1b89bdd1b84bc0ebbf2ba9d8fabc51c
nonle-dev <nonle.dev@gmail.com>
2023-07-17 17:59:11 +0700

    init project

---
+++ src/test/java/com/nonlee/SweetDreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonlee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetDreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
